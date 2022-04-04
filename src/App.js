fea95283d389adf67661ec9c2bf9c5b40c88adfa
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-04 18:10:07 +0530

    Counter test app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  let [age,setAge] = useState(30);
   return (
-    <div className="app">
-      
+    <div>
+      <h2>My age is = {age}</h2>
+      <button onClick={()=>{setAge(age += 1)}}>+</button>
+      <button onClick={()=>{setAge(age -= 1)}}>-</button>
     </div>
   );
 }
